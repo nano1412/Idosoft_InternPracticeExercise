bef8d2247ba0e8ff37d996a9753ad70d870a9027
nano1412_PC <54862667+nano1412@users.noreply.github.com>
2026-01-12 15:08:04 +0700

    prep file
    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '@/assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+
+      <div className='flex'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -28,6 +30,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      <h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
     </>
   )
 }
---
+++ src/Main/MainPage.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+// import './App.css'
+
+function MainPage() {
+  return(
+    <>
+      <h1>Hello</h1>
+    </>
+  )
+}
+
+export default MainPage
