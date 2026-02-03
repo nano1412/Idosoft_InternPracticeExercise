ea2edce920cbe03320c92779334401a5a59606a5
wachirawitkho-art <54862667+nano1412@users.noreply.github.com>
2026-02-03 14:41:32 +0700

    refactor:move import tailwind from app to index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
-import type { Bill } from "@/pages/components/types"
-import {BrowserRouter, Route, Routes } from 'react-router'
+import type { Bill } from "@/pages/components/types";
+import { BrowserRouter, Route, Routes } from "react-router";
 
-import './App.css'
-import Addbill from './pages/add-bill/AddBill.tsx'
-import Editbill from './pages/edit-bill/EditBill.tsx'
-import Managebill from './pages/manage-bill/ManageBill.tsx'
+import AddBill from "./pages/add-bill/AddBill.tsx";
+import EditBill from "./pages/edit-bill/EditBill.tsx";
+import ManageBill from "./pages/manage-bill/ManageBill.tsx";
 import Header from "./pages/header/Header.tsx";
 
 function App() {
@@ -13,21 +12,26 @@ function App() {
 
   return (
     <>
-    <div className="min-h-screen bg-gray-50">
+      <div className="bg-gray-50">
         <BrowserRouter>
-      <Header/>
-      <div className='py-8 px-12'>
-          <Routes>
-            <Route path = '/add' element = {<Addbill setBills = {setBills}/>}> </Route>
-            <Route path = '/edit/:id' element = {<Editbill bills = {bills} setBills = {setBills}/>}> </Route>
-            <Route path = '/' element = {<Managebill bills = {bills} setBills = {setBills}/>}> </Route>
-          </Routes>
+          <Header />
+          <div className="py-8 px-12">
+            <Routes>
+              <Route path="/add" element={<AddBill setBills={setBills} />} />
+              <Route
+                path="/edit/:id"
+                element={<EditBill bills={bills} setBills={setBills} />}
+              />
+              <Route
+                path="/"
+                element={<ManageBill bills={bills} setBills={setBills} />}
+              />
+            </Routes>
+          </div>
+        </BrowserRouter>
       </div>
-        </BrowserRouter> 
-    </div>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
