d513f160193024627ded7ecbd522967591684f0c
wachirawitkho-art <54862667+nano1412@users.noreply.github.com>
2026-02-04 12:51:07 +0700

    fix:header component
    -fix border of navlink too high
    
    -and center the title

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -4,13 +4,13 @@ const Header = () => {
   const linkClasses = ({ isActive }: { isActive: boolean }) =>
     `mx-2 cursor-pointer pb-1 ${
       isActive
-        ? "border-b-2 border-blue-500 text-blue-500"
+        ? "border-b-3 border-blue-500 text-blue-500"
         : "hover:text-blue-500"
     }`;
 
   return (
-    <div className="bg-white px-5 py-2 flex items-center justify-between border-b border-gray-200 drop-shadow-md font-bold">
-      <div>Simple Bill Management System</div>
+    <div className="bg-white px-5 pt-2 flex items-center justify-between border-b border-gray-200 drop-shadow-md font-bold">
+      <div className="pb-2">Simple Bill Management System</div>
 
       <div className="flex">
         <NavLink to="/" className={linkClasses}>
